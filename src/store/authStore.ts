5b3346d7af5f90841a1ccd4bb92be7609652bd03
Oksana Vakuliak <oksanabagrova19@gmail.com>
2026-08-16 13:02:58 +0300

    Add auth store with user and auth modal state

---
+++ src/store/authStore.ts
@@ -0,0 +1,28 @@
+import { create } from 'zustand';
+import type { User } from '@/types';
+
+export type AuthModal = 'login' | 'register';
+
+interface AuthState {
+  user: User | null;
+  isLoggedIn: boolean;
+  isAuthLoading: boolean;
+  authModal: AuthModal | null;
+  setUser: (user: User) => void;
+  clearUser: () => void;
+  setAuthLoading: (isAuthLoading: boolean) => void;
+  openAuthModal: (authModal: AuthModal) => void;
+  closeAuthModal: () => void;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  isLoggedIn: false,
+  isAuthLoading: false,
+  authModal: null,
+  setUser: (user) => set({ user, isLoggedIn: true }),
+  clearUser: () => set({ user: null, isLoggedIn: false }),
+  setAuthLoading: (isAuthLoading) => set({ isAuthLoading }),
+  openAuthModal: (authModal) => set({ authModal }),
+  closeAuthModal: () => set({ authModal: null }),
+}));
